171f17412f3912b0d882f8c093c46b113c21d856
Ilshat Shamilov <97913003+no1sy1@users.noreply.github.com>
2025-06-04 20:54:48 +0300

    Update main.cpp

--- 1 курс/Стартовые задачи/main.cpp
+++ 1 курс/Стартовые задачи/main.cpp
@@ -1,1 +1,38 @@
+//1
+#include <iostream>
+#include <fstream>
 
+using namespace std;
+
+int main() {
+    ifstream in("input.cpp");
+    ofstream out("output.txt");
+    
+    int balance = 0;
+    char k;
+    
+    while (in.get(k)) {
+        if (k == '{') {
+            balance++;
+        }
+        else if (k == '}') {
+            balance--;
+            if (balance < 0) {
+                balance = 0;
+            }
+        }
+    }
+
+    if (balance > 0) {
+        cout << "Фигурные скобки не закрыты" << endl;
+        out << "Фигурные скобки не закрыты" << endl;
+    }
+    else if (balance == 0) {
+        cout << "Все фигурные скобки закрыты" << endl;
+        out << "Все фигурные скобки закрыты" << endl;
+    }
+
+    in.close();
+    out.close();
+    return 0;
+}
